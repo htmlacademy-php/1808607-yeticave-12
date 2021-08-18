89405d6205aee594643518bb9d4bca2777f6b597
Анастасия <rushkova.aa@gmail.com>
2021-08-18 16:19:05 +0300

    Внесены доработки по условию и исправлены мелкие недочеты

--- index.php
+++ index.php
@@ -28,9 +28,9 @@ $user_name = 'Anastasiia'; // укажите здесь ваше имя
 
         <nav class="user-menu">
 
-	<?php if ($is_auth == True): ?>
+	<?php if ($is_auth === true): ?>
 		<div class="user-menu__logged"> 
-		<p><?=$user_name;?></p>   
+		<p><?= $user_name; ?></p>   
 		<a class="user-menu__bets" href="pages/my-bets.html">Мои ставки</a>   
 		<a class="user-menu__logout" href="#">Выход</a>   
 		</div> 
